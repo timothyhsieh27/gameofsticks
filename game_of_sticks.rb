920914494c8ae97e9e6ab8235c5127c4b556e2fe
Tim <timothyhsieh27@gmail.com>
2016-09-20 20:11:58 -0400

    Updated clean version - player one and two now alternate turns without nomenclature.

--- game_of_sticks.rb
+++ game_of_sticks.rb
@@ -1,4 +1,3 @@
-
 def game_start
   loop do
     puts "Welcome to the Game of Sticks! Choose how many sticks the table starts with(10-100): "
@@ -12,46 +11,27 @@ def game_start
   end
 end
 
-def first_turn(first_take)
+def take_sticks(player_take)
   loop do
-    puts "There are #{@stick_number} on the board."
-    puts "Player 1: How many sticks will you take (1-3)?"
-    first_take = gets.chomp.to_i
-    if 1 <= first_take && first_take <= 3
-      @stick_number = @stick_number - first_take
+    puts "There are #{@stick_number} sticks on the board."
+    puts "Player: How many sticks will you take (1-3)?"
+    player_take = gets.chomp.to_i
+    if 1 <= player_take && player_take <= 3
+      @stick_number = @stick_number - player_take
       puts "There are now #{@stick_number} sticks remaining."
         break
     else
       puts "Invalid input. Please enter a number between 10 and 100: "
-      end
-  end
-end
-
-def second_turn(second_take)
-  loop do
-    puts "There are #{@stick_number} on the board."
-    puts "Player 2: How many sticks will you take (1-3)?"
-    second_take = gets.chomp.to_i
-    if 1 <= second_take && second_take <= 3
-      @stick_number = @stick_number - second_take
-      puts "There are now #{@stick_number} sticks remaining."
-        break
-    else
-      puts "Invalid input. Please enter a number between 10 and 100: "
-      end
+    end
   end
 end
 
 def main
-  first_take =
-  second_take =
-
+  player_take =
   game_start
-
   loop do
     if @stick_number > 0
-      first_turn(first_take)
-      second_turn(second_take)
+      take_sticks(player_take)
     else
       puts "Game over! You lose!"
       break
@@ -62,21 +42,6 @@ end
 class Sticks
   def initialize
     @stick_number = stick_number
-  end
-
-  def take_sticks(player_take)
-    loop do
-      puts "There are #{@stick_number} on the board."
-      puts "Player: How many sticks will you take (1-3)?"
-      player_take = gets.chomp.to_i
-      if 1 <= player_take && player_take <= 3
-        @stick_number = @stick_number - second_take
-        puts "There are now #{@stick_number} sticks remaining."
-          break
-      else
-        puts "Invalid input. Please enter a number between 10 and 100: "
-      end
-    end
   end
 end
 
